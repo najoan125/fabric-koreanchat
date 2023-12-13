18112e74f25f04f9af863ad08df8c82a51e7f0f1
Najoan <68422289+najoan125@users.noreply.github.com>
2023-12-13 18:02:36 +0900

    Indicator bug fix(change int color)

--- src/main/java/com/hyfata/najoan/koreanpatch/util/Indicator.java
+++ src/main/java/com/hyfata/najoan/koreanpatch/util/Indicator.java
@@ -5,8 +5,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.Text;
 
-import java.util.Objects;
-
 public class Indicator {
     static MinecraftClient client = MinecraftClient.getInstance();
     static Text KOREAN = Text.literal("한");
@@ -20,17 +18,17 @@ public class Indicator {
 
         if (KoreanPatchClient.KOREAN) {
             context.fill(x, y, x+12, y+12, -65536);
-            context.fill(x+1, y+1, x+11, y+11, Objects.requireNonNull(client).options.getTextBackgroundColor(-587202560));
+            context.fill(x+1, y+1, x+11, y+11, -16777216);
             drawCenteredText(context, KOREAN, x+6, y+2);
         }
         else{
             context.fill(x, y, x+12, y+12, -16711936);
-            context.fill(x+1, y+1, x+11, y+11, Objects.requireNonNull(client).options.getTextBackgroundColor(-587202560));
+            context.fill(x+1, y+1, x+11, y+11, -16777216);
             drawCenteredText(context, ENGLISH, x+6, y+2);
         }
     }
 
     private static void drawCenteredText(DrawContext context, Text text, int x, int y) {
-        context.drawText(client.textRenderer, text, x - client.textRenderer.getWidth(text) / 2, y, 16777215, false);
+        context.drawText(client.textRenderer, text, x - client.textRenderer.getWidth(text) / 2, y, -1, false);
     }
 }
