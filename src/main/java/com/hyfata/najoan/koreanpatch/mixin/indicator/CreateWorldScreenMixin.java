9b0c0c870f7ab14c71b1232a204310466ee9ca68
Najoan <68422289+najoan125@users.noreply.github.com>
2024-07-27 22:19:44 +0900

    backport 1.19.3

--- src/main/java/com/hyfata/najoan/koreanpatch/mixin/indicator/CreateWorldScreenMixin.java
+++ src/main/java/com/hyfata/najoan/koreanpatch/mixin/indicator/CreateWorldScreenMixin.java
@@ -1,15 +1,11 @@
 package com.hyfata.najoan.koreanpatch.mixin.indicator;
 
 import com.hyfata.najoan.koreanpatch.client.KoreanPatchClient;
-import com.hyfata.najoan.koreanpatch.mixin.accessor.CreateWorldScreenGameTabAccessor;
-import com.hyfata.najoan.koreanpatch.mixin.accessor.TabNavigationWidgetInvoker;
 import com.hyfata.najoan.koreanpatch.util.animation.AnimationUtil;
 import com.hyfata.najoan.koreanpatch.util.Indicator;
 import com.hyfata.najoan.koreanpatch.util.TextFieldWidgetUtil;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
-import net.minecraft.client.gui.tab.Tab;
-import net.minecraft.client.gui.widget.TabNavigationWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
@@ -22,29 +18,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = {CreateWorldScreen.class})
 public class CreateWorldScreenMixin extends Screen {
+    @Shadow private TextFieldWidget levelNameField;
+
+    @Shadow private boolean moreOptionsOpen;
+
     protected CreateWorldScreenMixin(Text title) {
         super(title);
     }
-
-    @Shadow
-    private TabNavigationWidget tabNavigation;
 
     @Unique
     AnimationUtil animationUtil = new AnimationUtil();
 
     @Inject(at = {@At(value = "RETURN")}, method = {"render"})
     private void addCustomLabel(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        TabNavigationWidgetInvoker tabInvoker = (TabNavigationWidgetInvoker) tabNavigation;
-        Tab currentTab = tabInvoker.getTabs().get(tabInvoker.currentTabIndex());
-
-        if (currentTab instanceof CreateWorldScreen.GameTab) {
+        if (!moreOptionsOpen) {
             KoreanPatchClient.bypassInjection = false;
-            CreateWorldScreenGameTabAccessor gameTabAccessor = (CreateWorldScreenGameTabAccessor) currentTab;
-            TextFieldWidget worldNameField = gameTabAccessor.getWorldNameField();
             Text text = Text.translatable("selectWorld.enterName");
 
-            float x = TextFieldWidgetUtil.getCursorXWithText(worldNameField, text, worldNameField.getX()) + 4;
-            float y = TextFieldWidgetUtil.calculateIndicatorY(worldNameField);
+            float x = TextFieldWidgetUtil.getCursorXWithText(levelNameField, text, levelNameField.getX()) + 4;
+            float y = TextFieldWidgetUtil.calculateIndicatorY(levelNameField);
 
             animationUtil.init(x - 4, 0);
             animationUtil.calculateAnimation(x, 0);
